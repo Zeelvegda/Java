2a470c45107ec694f96354a153eb9c0aaa1bfd70
Zeelvegda <140957945+Zeelvegda@users.noreply.github.com>
2024-04-09 21:13:07 +0530

    Add files via upload

---
+++ LAb 1/HelloWorld.java
@@ -0,0 +1,6 @@
+//lab1_3
+public class HelloWorld{
+    public static void main (String[] args){
+        System.out.println("HelloWorld");
+    }
+}
---
+++ LAb 1/Input_Arg.java
@@ -0,0 +1,10 @@
+
+//Problem 4
+public class Input_Arg {
+    public static void main(String[] args) {
+      String p = args[0];
+      int a = Integer.parseInt(args[1]);
+      System.out.println(p + " " + a);
+        
+    }
+}
